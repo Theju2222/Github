71f5a3ff525f17e8195d46b72f13b0b06a376d84
Thejas B M <108926913+Theju2222@users.noreply.github.com>
2025-02-26 11:27:08 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  images: {
+    unoptimized: true,  // Disable image optimization (useful for static sites)
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
